10290f00b7fa8fc5882670a004d2fd46f917e29d
komisdaria <darijana701@gmail.com>
2021-05-11 22:59:22 +0300

    some styles

--- routes/index.js
+++ routes/index.js
@@ -5,18 +5,19 @@ const User = require('../models/user.model');
 const router = Router();
 
 router.get('/', async (req, res) => {
+  // код для отображения только постов этого юзера
   // нижние 2 не нужны тк найдут всех юзеров и все их посты, а нужны только этого юзера
   // const memo = await Memo.find().populate('author');
   // const users = await User.find();
   const userOne = await User.findOne({ username: req.session.username });
   // const allUserMemo = await Memo.find({ author: users.id }).populate('author');
-  const usersAllMemo = await Memo.find({ username: req.session.username });
+  const usersAllMemo = await Memo.find({ author: userOne.id }).populate('author');
   // req.session.users = users;
   // req.session.memo = memo;
   req.session.userOne = userOne;
   req.session.usersAllMemo = usersAllMemo;
-  console.log(userOne);
-  console.log(usersAllMemo);
+  // console.log(userOne);
+  // console.log(usersAllMemo);
   res.render('index', { usersAllMemo, userOne });
 });
 
